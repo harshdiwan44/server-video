50520e58b836bcc9e8bdca76390f649e4574e291
harshdiwan44 <97082579+harshdiwan44@users.noreply.github.com>
2022-06-18 14:16:59 +0530

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -4,8 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://52.66.62.46:5000/');
-// const socket = io('https://warm-wildwood-81069.herokuapp.com');
+// const socket = io('http://52.66.62.46:5000/');
+const socket = io('https://warm-wildwood-81069.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
